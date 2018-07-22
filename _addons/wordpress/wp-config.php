3df5e4b686417d4ba16cfc20f4ad732b8bb0ded0
ghennessey <greg@bluebatgames.com>
2018-07-22 07:42:57 -0700

    Allowing CORS in WP Config

--- _addons/wordpress/wp-config.php
+++ _addons/wordpress/wp-config.php
@@ -87,3 +87,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+// allow CORS
+header("Access-Control-Allow-Origin: *");
