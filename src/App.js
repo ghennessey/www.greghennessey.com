dc563a34e31dcc05e8c4afdd169d632185b157aa
ghennessey <greg@bluebatgames.com>
2018-08-09 14:54:59 -0700

    Data fetching working now

--- src/App.js
+++ src/App.js
@@ -2,44 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var WPAPI = require( 'wpapi' );
-var wp = new WPAPI({ endpoint: 'http://www.greghennessey.com/wp-json' });
+//var WPAPI = require( 'wpapi' );
+//var wp = new WPAPI({ endpoint: 'http://www.greghennessey.com/wp-json' });
 
-var PAGE_DATA = {
-  PAGE_TITLE: ''
-}
-
-
-wp.pages().get(function( err, data ) {
-    if ( err ) {
-      return console.log("Cannot find the page you're looking for");
-    }
-    PAGE_DATA.PAGE_TITLE = data[0].title.rendered;
-    console.log("Page title is " + PAGE_DATA.PAGE_TITLE);
-});
-
+const API = "http://www.greghennessey.com/wp-json";
+const PAGES = "/wp/v2/pages";
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
+      hits: [],
+      //REMOVE - Page title needs to be moved into another lower component
       pageTitle: ''
     };
   }
 
   componentDidMount() {
 
-    fetch('http://www.greghennessey.com/wp-json')
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      console.log(data);
-      this.setState({
-        pageTitle: data.name
-      });
-    });
-
+    //Asyncronously get data and set the results
+    fetch(API + PAGES)
+      .then(results => results.json())
+      .then(data => {
+        //Grab the site JSON data and store it in a state for futre use
+        //REMOVE - remove this log later
+        console.log(data);
+        this.setState({hits: data});
+      }).then(
+        //REMOVE - This section is just here to set an example pageTitle
+        //later I would like to move this down into deeper functions and components and just
+        //get site data at the high level
+        data => {
+          this.setState({
+            pageTitle: this.state.hits[0].title.rendered
+          });
+        }
+      );
   }
 
   render() {
